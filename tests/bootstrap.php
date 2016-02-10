b44d50b276ab831fafd1ab99dd07e7965dd5e263
Stephan Schmitz <eyecatchup@gmail.com>
2016-02-10 23:47:51 +0100

    Fix test bootstrap file import

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: stephan.schmitz
- * Date: 10.02.2016
- * Time: 11:50
+ * This file is part of Eyecatchup/GetGitStats.
+ *
+ * @package    GetGitStats
+ * @link       https://github.com/eyecatchup/GetGitStats Project website
+ * @author     Stephan Schmitz <eyecatchup@gmail.com>
+ * @copyright  Copyright (C) 2016 Stephan Schmitz, https://eyecatchup.github.io/
+ * @license    http://eyecatchup.mit-license.org/ MIT License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 require_once 'vendor/autoload.php';
-require_once 'src/Eyecatchup/GetGitStats.php';
+require_once 'src/Eyecatchup/GetGitStats/GetGitStats.php';
